02239f3cfeb063cef1cc88654988304657fe0ae4
Kornel Łaba <kornellaba44@gmail.com>
2025-10-28 21:33:25 +0100

    1

--- hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\korne\projects\rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-C:\Users\korne\projects\rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-src\main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\korne\projects\rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\korne\projects\rust\hello_cargo\src\main.rs
