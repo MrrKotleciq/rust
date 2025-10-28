9f059dcc5c92eb03c46a1466727026dd76540938
Kornel Łaba <kornellaba44@gmail.com>
2025-10-28 21:35:48 +0100

    hello_cargo

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// important commands:
+
+// cargo new -> creates a new Rust project with this main.rs file as the entry point.
+// cargo build -> compiles the project
+// cargo build --release -> compiles the project with optimizations for release, but takes longer to compile
+// cargo run -> compiles and runs the project in one step
+// cargo check -> checks the code for errors without producing an executable, faster than build
+
+// less important commands:
+
+// cargo fmt -> formats the code according to Rust's style guidelines
+// cargo clippy -> runs a linter to catch common mistakes and improve code quality
+// cargo test -> runs the tests defined in the project
+// cargo doc -> generates documentation for the project based on comments in the code
+// cargo update -> updates the dependencies listed in Cargo.toml to the latest versions allowed by the
---
+++ hello_cargo/target/debug/deps/hello_cargo-62853b5a06543bf0.d
@@ -0,0 +1,5 @@
+C:\Users\korne\projects\rust\hello_cargo\target\debug\deps\hello_cargo-62853b5a06543bf0.d: src\main.rs
+
+C:\Users\korne\projects\rust\hello_cargo\target\debug\deps\libhello_cargo-62853b5a06543bf0.rmeta: src\main.rs
+
+src\main.rs:
