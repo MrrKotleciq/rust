9b78145aafa684dab4ca4a4c03434eb32e3a31b1
Kornel Łaba <kornellaba44@gmail.com>
2025-10-29 20:58:54 +0100

    1

--- guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-C:\Users\korne\projects\rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
-
-C:\Users\korne\projects\rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
-
-src\main.rs:
