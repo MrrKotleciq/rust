6110007a29674d9ca28bb1a4d409d9e5c323f3da
Kornel Łaba <kornellaba44@gmail.com>
2025-11-07 11:23:25 +0100

    Refactor main to use plus_one function
    Introduced a new plus_one function and updated main to use it for computing the value of x. Removed the previous block expression and improved code clarity.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -9,10 +9,19 @@
 //     another_function(5, 'h');
 // }
 
+// fn main() {
+//     let y = {
+//         let x = 3;
+//         x + 1
+//     };
+//     println!("The value of y is {y}");
+//}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
 fn main() {
-    let y = {
-        let x = 3;
-        x + 1
-    };
-    println!("The value of y is {y}");
+    let x = plus_one(5);
+    println!("The value of x is: {x}")
 }
